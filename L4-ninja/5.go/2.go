030b80ff189396f83cb9f0baeebee09e92d42c90
Sree Kolli <kollisree7@gmail.com>
2021-07-28 23:40:09 -0700

    readme

--- L4-ninja/5.go/2.go
+++ L4-ninja/5.go/2.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 
-var = []int{1, 2, 4, 8, 16, 32, 64, 128}
-for i 
+	x := [9]int{1, 2, 4, 8, 16, 32, 64, 128} // I assigned a value to x and next to the square brackets
+	// the type which is int.
+	for i, v := range x { // I used i which is the index and the v is varant i used to assign to range the number
+		// x is the slice and I am still not sure on what it does even after I watched the videos?
+		//
+		fmt.Println(i, v)
+	}
 
-//Incomplete code
+	//Incomplete code
 
 }
