6bf56b7129a9d37513fe8ed5888fea681d76c653
Sandeep Chandu <9066963+chanduusc@users.noreply.github.com>
2021-07-30 18:27:59 -0700

    Comments -7302021

--- L3-Ninja/3.go
+++ L3-Ninja/3.go
@@ -26,3 +26,5 @@ func main() {
 	// declare variable and assign your year of birth to it and then use for less than present as condition for for to print years and increment it
 
 }
+
+//Code cleanup is needed 
