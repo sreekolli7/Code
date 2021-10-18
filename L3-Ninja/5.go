1e848e36379f6e5e805aea7ae6849471ef3129e6
Sree Kolli <kollisree7@gmail.com>
2021-10-18 01:06:19 -0700

    readme

--- L3-Ninja/5.go
+++ L3-Ninja/5.go
@@ -1,17 +1,12 @@
 package main
 
-//Print out the remainder (modulus) which is found for each number between 10 and 100 when it is divided by 4.
-
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	for x := 10; x <= 100; x++ { // I am confused on how to do the for loop for this but I searched up
-		// for the print statemnt I used println and I used percent which is
-		//used to divide the numbers by 4
-		fmt.Println(x, x%4)
-	}
+	x := "Egg, Elephant, Eagel, "
+	fmt.Println(x)
+	firstword := x[0:1]
+	fmt.Println(firstword)
 }
-
-// loop from 10 to 100 and print %4 of every iteration
-
-//Looks good
